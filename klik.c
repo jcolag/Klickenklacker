c007e1bc216cad5d0f6d0f85ee3cda4025e4d0ed
John Colagioia <john@colagioia.net>
2014-06-11 19:06:47 -0400

    Extract header information

--- klik.c
+++ klik.c
@@ -18,54 +18,7 @@
 #include <mem.h>
 #include <dos.h>
 #endif                                  /* __MSDOS__ */
-
-/* Directional Constants */
-#define LEFT   (1)
-#define UP     (2)
-#define RIGHT  (3)
-#define DOWN   (4)
-#define STOP   (0)
-
-/* Object Type Constants */
-#define STA    (0)
-#define MOT    (1)
-#define MOV    (2)
-
-/* The force, lowercase 'f' */
-#define GRAV   (1)
-
-/* Objects in the warehouse */
-struct  thing {
-    char   display;                     /* The image displayed */
-    int    type;                        /* The kind of object */
-    int    pushed;                      /* How fast it must move */
-    int    motivation;                  /* How fast it naturally moves */
-    int    direction;                   /* Current direction of motion */
-    int    freefall;                    /* Does gravity apply? */
-    int    checked;                     /* Administrative, to move only once */
-};
-
-struct thing **warehouse;               /* The main board */
-int     height,                         /* Height of board */
-        width,                          /* Width of board */
-        ansi,                           /* ANSI terminal? */
-        ltrunc,                         /* Line length */
-        top;                            /* Screen height */
-
-/* Grid maintenance functions */
-struct thing **buildWarehouse(int x, int y);
-void condemnWarehouse(struct thing **warehouse, int x);
-void initializeWarehouse(struct thing **warehouse, int x, int y);
-void fillWarehouse(struct thing **warehouse, int x, int y);
-
-/* Administrative functions */
-int readFile(char *filename, struct thing **warehouse);
-void showGrid(struct thing **warehouse, int x, int y);
-
-/* Actual RUBE-ing functions */
-void cycle(struct thing ***warehouse, int x, int y);
-int moveThing(struct thing **new, struct thing **old, int x, int y);
-int priority(struct thing *object);
+#include "klik.h"
 
 int main(int argc, char *argv[]) {
     int     cont = 1,
